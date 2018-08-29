fdf2a6fa73695d515acb125c4c731e2a6b220029
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-29 13:22:54 -0400

    Writing and reading from a file

--- TALogIODrill/Program.cs
+++ TALogIODrill/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TALogIODrill
 {
@@ -10,6 +11,14 @@ namespace TALogIODrill
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Give me a number");
+            string num = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Keenan\Desktop\IOtest.txt", num);
+            string text = File.ReadAllText(@"C:\Users\Keenan\Desktop\IOtest.txt");
+            Console.WriteLine(text);
+
+            Console.ReadLine();
         }
     }
 }
